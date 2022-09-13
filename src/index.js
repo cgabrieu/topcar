3b4715480b80dec97c1570f8375990afb5f4cb6d
Carlos Gabriel <carlosgabrielpc@live.com>
2022-09-13 11:08:16 -0300

    feat(styles): add reset css and general styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './assets/styles/reset.css';
+import './assets/styles/general-styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
