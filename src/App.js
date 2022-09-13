a1fb9eb4e88ffecf0770eb8d0273b607ebe4f476
Carlos Gabriel <carlosgabrielpc@live.com>
2022-09-13 11:45:14 -0300

    chore(package/eslint): solve absolute imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import BaseContainer from './components/BaseContainer';
-import Home from './pages/Home';
+import BaseContainer from 'components/BaseContainer';
+import Home from 'pages/Home';
 
 export default function App() {
 	return (
