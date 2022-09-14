6a86d63f51e9641f481c2d26a264abb50f44a499
Carlos Gabriel <carlosgabrielpc@live.com>
2022-09-14 07:35:11 -0300

    feat(pagination-component): change background color on select

--- src/components/Pagination.component.js
+++ src/components/Pagination.component.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Pagination({ next, prev, goTo, maxPage }) {
-
+export default function Pagination({ currentPage, next, prev, goTo, maxPage }) {
 	return (
 		<PaginationContainer>
 			<ButtonArrow onClick={prev}>{`<`}</ButtonArrow>
 			{[...Array(maxPage + 1).keys()].slice(1).map((n) => (
-				<GoToLabel key={n} onClick={() => goTo(n)}>
+				<GoToLabel
+					key={n}
+					isSelected={currentPage === n}
+					onClick={() => goTo(n)}
+				>
 					{n}
 				</GoToLabel>
 			))}
@@ -19,14 +22,25 @@ export default function Pagination({ next, prev, goTo, maxPage }) {
 const PaginationContainer = styled.li`
 	display: flex;
 	justify-content: space-around;
-	margin: 2px 10%;
+	margin: 10px 10% 3px 10%;
 `;
 
 const ButtonArrow = styled.div`
-
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 15px;
+	font-weight: 500;
+	color: var(--smoke-color);
+	border: 1px solid var(--light-text-color);
+	border-radius: 50%;
+	width: 30px;
+	height: 30px;
 `;
 
-const GoToLabel = styled.div`
-	cursor: pointer;
-
+const GoToLabel = styled(ButtonArrow)`
+	font-size: 13px;
+	background-color: ${({ isSelected }) => isSelected && 'var(--secondary-text-color)'};
+	color: ${({ isSelected }) => isSelected && 'var(--light-color)'};
 `;
