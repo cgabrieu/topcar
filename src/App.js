cf0ce086f184e41d2003f13d6124333bba493973
Carlos Gabriel <carlosgabrielpc@live.com>
2022-09-20 07:12:53 -0300

    feat(routes): create routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import BaseContainer from 'components/Base.container';
 import Home from 'pages/Home';
+import CarDetail from 'pages/CarDetail';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: '/car/:carId',
+		element: <CarDetail />,
+	},
+]);
 
 export default function App() {
 	return (
 		<BaseContainer>
-			<Home />
+			<RouterProvider router={router} />
 		</BaseContainer>
 	);
 }
