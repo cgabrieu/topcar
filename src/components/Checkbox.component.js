d515f709c581b8fb687d2f612069e795b4fee04a
Carlos Gabriel <carlosgabrielpc@live.com>
2022-09-20 10:39:52 -0300

    feat(checkbox): separete componente ¨

---
+++ src/components/Checkbox.component.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as MarkIcon } from 'assets/icons/mark.svg';
+import { ReactComponent as DashIcon } from 'assets/icons/dash.svg';
+
+export default function Checkbox({ isSelected, handleOnSelect }) {
+	return (
+		<CheckboxContainer onClick={handleOnSelect}>
+			{isSelected && <MarkIcon />}
+			{isSelected === null && <DashIcon />}
+		</CheckboxContainer>
+	);
+}
+
+const CheckboxContainer = styled.div`
+	cursor: pointer;
+	min-width: 25px;
+	height: 25px;
+	border: 3px solid var(--main-text-color);
+`;
