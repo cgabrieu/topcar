bbc8cec5045aef1fd6d2e0591f8c6acefd148ada
Carlos Gabriel <carlosgabrielpc@live.com>
2022-09-23 07:58:37 -0300

    chore(app): provider at App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
 import CarDetail from 'pages/CarDetail';
 import BaseContainer from 'components/Base.container';
+import ListCarsProvider from 'contexts/ListCars.context';
 
 export default function App() {
 	return (
-		<BrowserRouter>
-			<BaseContainer>
-				<Routes>
-					<Route index element={<Home />} />
-					<Route path="car/:carId" element={<CarDetail />} />
-				</Routes>
-			</BaseContainer>
-		</BrowserRouter>
+		<ListCarsProvider>
+			<BrowserRouter>
+				<BaseContainer>
+					<Routes>
+						<Route index element={<Home />} />
+						<Route path="car/:carId" element={<CarDetail />} />
+					</Routes>
+				</BaseContainer>
+			</BrowserRouter>
+		</ListCarsProvider>
 	);
 }
